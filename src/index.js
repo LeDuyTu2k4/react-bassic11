d4a39d928f1a324e521f3a730982ff72df3d2e5d
LeDuyTu2k4 <tu26052004@gmail.com>
2023-04-18 09:35:18 +0700

    first commit leduytu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
-import './styles/global.scss'
+import './styles/global.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { withRouter } from "react-router";
 import Color from "../HOC/Color";
+import logo from '../../assets/images/1143430.jpg';
+import '../../styles/images.scss';
+
 class Home extends React.Component {
 
     componentDidMount() {
@@ -12,11 +15,19 @@ class Home extends React.Component {
     render() {
         console.log('>>>check props:', this.props)
         return (
-            <div>
-                Hello world from Homepage
-            </div>
+            <>
+                <div>
+                    Hello world from Homepage
+                </div>
+                <div className="logo">
+                    <img className="logo-1" src={logo} />
+                </div>
+            </>
         )
     }
 }
+
+
+
 // export default withRouter(Home);
 export default Color(Home);
